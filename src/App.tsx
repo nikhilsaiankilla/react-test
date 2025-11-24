77a498c964551501a446f8007f2973d352326a7f
nikhil sai ankilla <nikhilsaiankilla@gmail.com>
2025-11-24 14:22:40 +0530

    fixed issue

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { useVisly, VislyProvider, VislyRouteTracker } from "visly-sdk/react";
+import { useVisly, VislyRouteTracker } from "visly-sdk/react";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,32 +10,30 @@ function App() {
 
   return (
     <>
-      <VislyProvider projectId="PROJECT-12356789">
-        <VislyRouteTracker pathname={window.location.pathname} />
-        <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => {
-            setCount((count) => count + 1)
-            visly.track("button_clicked", { label: "Sign Up" });
-          }}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
+      <VislyRouteTracker pathname={window.location.pathname} />
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => {
+          setCount((count) => count + 1)
+          visly.track("button_clicked", { label: "Sign Up" });
+        }}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </VislyProvider>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { VislyProvider } from 'visly-sdk/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <VislyProvider projectId="PROJECT-12356789">
+      <App />
+    </VislyProvider>
   </StrictMode>,
 )
